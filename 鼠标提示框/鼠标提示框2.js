01a32608a591ede60794ca1b896d90544ac1365a
xaiozhang <1315464487@qq.com>
2020-06-19 22:28:05 +0800

    619

---
+++ 鼠标提示框/鼠标提示框2.js
@@ -0,0 +1,6 @@
+document.getElementById("J-remmber").onmouseover = function (){
+    document.getElementById("J-tips").style.display="block"
+}
+document.getElementById("J-remmber").onmouseout = function (){
+    document.getElementById("J-tips").style.display="none"
+}
